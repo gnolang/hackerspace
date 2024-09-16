dde7bc1269b4b6e46c0c6a5fe36bbd4576e06caf
albttx <contact@albttx.tech>
2024-09-16 13:30:56 +0100

    chore: example for a snapshot node

---
+++ snapshots-node/scripts/snapshots.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+HEIGHT=$(curl -s gnoland:26657/status | jq -r '.result.sync_info.latest_block_height')
+echo "[INFO] Snapshotting at height: ${HEIGHT}"
+
+echo "[INFO] Stopping gnoland..."
+docker compose stop gnoland
+
+FILE="gnoland-${HEIGHT}-${CHAIN_ID}.tar.lz4"
+
+tar cvf - -C ${DAEMON_HOME} wal db | lz4 > $FILE
+
+mc cp --attr x-amz-acl=public-read --tags="type=snapshot" ${FILE} "s3/${MC_BUCKET}/${CHAIN_ID}/${FILE}"; echo
+
+rm -vf ${FILE}
+
+echo "[INFO] Starting gnoland ..."
+docker compose start gnoland
