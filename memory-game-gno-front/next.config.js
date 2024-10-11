3809383da39e62a3d1e0ee2af7e6569975546472
matijamarjanovic <mmarjanovic11121rn@raf.rs>
2024-10-11 22:31:24 +0200

    Update project, get it ready for github pages

--- memory-game-gno-front/next.config.js
+++ memory-game-gno-front/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  output: 'export'
 };
 
 module.exports = nextConfig; // Use CommonJS syntax instead of ES module syntax
