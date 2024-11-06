ad57a69b82935995379b8da21c390e363242f10f
albttx <contact@albttx.tech>
2024-11-06 10:08:08 +0000

    chore: add README, update cli

--- snapshots-node/scripts/snapshots.sh
+++ snapshots-node/scripts/snapshots.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+DAEMON_HOME="/gnoroot/gnoland-data"
+
 HEIGHT=$(curl -s gnoland:26657/status | jq -r '.result.sync_info.latest_block_height')
 echo "[INFO] Snapshotting at height: ${HEIGHT}"
 
@@ -10,7 +12,7 @@ FILE="gnoland-${HEIGHT}-${CHAIN_ID}.tar.lz4"
 
 tar cvf - -C ${DAEMON_HOME} wal db | lz4 > $FILE
 
-mc cp --attr x-amz-acl=public-read --tags="type=snapshot" ${FILE} "s3/${MC_BUCKET}/${CHAIN_ID}/${FILE}"; echo
+mcli cp --attr x-amz-acl=public-read --tags="type=snapshot" ${FILE} "s3/${MC_BUCKET}/${CHAIN_ID}/${FILE}"; echo
 
 rm -vf ${FILE}
 
